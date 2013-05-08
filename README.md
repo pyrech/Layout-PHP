42156ff586c3a131eaba83a27444ef6da97ad95b
Loick Piera <pyrech@gmail.com>
2013-05-09 01:52:49 +0200

    Add a new way to use the class

--- README.md
+++ README.md
@@ -3,25 +3,46 @@ Layout-PHP
 
 A PHP class that wrap and render an HTML Layout. You can customize the doctype, meta, stylesheet, scripts, etc. 
 
-See an example below :
+The class can be used in two ways. You can either specify your settings in the render method or add each element in head section in the order you want.
 
+See an example below :
 
     $content = '<h1>Hello World</h1>';
 
-    $opts = array('doctype' => Layout::DOCTYPE_HTML5,
+    $layout1 = \Pyrech\Layout::getInstance();
+    $layout2 = \Pyrech\Layout::getInstance();
+
+    $opts = array('doctype' => \Pyrech\Layout::DOCTYPE_HTML5,
                   'meta'    => array('charset'            => 'utf-8',
                                      'description'        => 'Description of your page',
                                      'robot'              => 'index',
                                      'http-equiv:refresh' => '60'),
                   'title'   => 'My wonderful title',
-                  'class'   => array('some-class', 'another-class'),
                   'icon'    => array('png' => '/favicon.png',
                                      'ico' => '/favicon.ico'),
                   'styles'  => array('/my-stylesheet.css',
                                      array('media' => 'print',
                                            'href'  => '/print.css')),
-                  'scripts' => array('/my-js.css',
+                  'scripts' => array('/my-javascript.js',
                                      array('internal' => 'alert("Hello World!");')),
-                  'defer'   => true);
+                  'defer'   => true,
+                  'class'   => array('some-class', 'another-class'));
                   
-    echo Layout::getInstance()->render($content, $opts);
+    echo $layout1->render($content, $opts)."\n\n\n\n";
+
+    $layout2->setDoctype(\Pyrech\Layout::DOCTYPE_HTML5);
+    $layout2->addMeta('charset', 'utf-8');
+    $layout2->addTitle('My wonderful title');
+    $layout2->addMeta('description', 'Description of your page');
+    $layout2->addMeta('robot', 'index');
+    $layout2->addMeta('http-equiv:refresh', '60');
+    $layout2->addIcon('png', '/favicon.png');
+    $layout2->addIcon('ico', '/favicon.ico');
+    $layout2->addStyle('/my-stylesheet.css');
+    $layout2->addStyle(array('media' => 'print',
+                             'href'  => '/print.css'));
+    $layout2->addScript('/my-javascript.js', true);
+    $layout2->addScript(array('internal' => 'alert("Hello World!")'));
+    $layout2->addBodyClass(array('some-class', 'another-class'));
+
+    echo $layout2->render($content);
--- src/Layout.php
+++ src/Layout.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Pyrech\Layout;
+namespace Pyrech;
 
 /**
  * Wrapper of the HTML Layout
@@ -27,6 +27,18 @@ class Layout {
   const DOCTYPE_XHTML1_0_FRAMESET     = 6;
   const DOCTYPE_HTML5                 = 7;
 
+  public static $doctypes_available = array(DOCTYPE_HTML4_01_TRANSITIONAL,
+                                            DOCTYPE_HTML4_01_STRICT,
+                                            DOCTYPE_HTML4_01_FRAMESET,
+                                            DOCTYPE_XHTML1_0_TRANSITIONAL,
+                                            DOCTYPE_XHTML1_0_STRICT,
+                                            DOCTYPE_XHTML1_0_FRAMESET,
+                                            DOCTYPE_HTML5);
+
+  private $doctype = self::DOCTYPE_HTML5;
+  private $body_classes = "";
+  private $head = '';
+
   public function __construct() { }
 
   /**
@@ -40,37 +52,60 @@ class Layout {
   }
 
   /**
-   * Return the doctype according to the constant passed to the function
+   * Return the doctype tag according to the doctype setted
    * 
-   * @param int $version
    * @return string
    */
-  public function getDoctype($version = self::DOCTYPE_HTML5) {
-    $doctype = '';
-    switch($version) {
+  public function getDoctypeTag() {
+    $str = '';
+    switch($this->doctype) {
       case self::DOCTYPE_HTML4_01_TRANSITIONAL:
-       $doctype = '<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">';
+       $str = '<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">';
        break;
       case self::DOCTYPE_HTML4_01_STRICT:
-       $doctype = '<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">';
+       $str = '<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">';
        break;
       case self::DOCTYPE_HTML4_01_FRAMESET:
-       $doctype = '<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Frameset//EN" "http://www.w3.org/TR/html4/frameset.dtd">';
+       $str = '<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Frameset//EN" "http://www.w3.org/TR/html4/frameset.dtd">';
        break;
       case self::DOCTYPE_XHTML1_0_TRANSITIONAL:
-       $doctype = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">';
+       $str = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">';
        break;
       case self::DOCTYPE_XHTML1_0_STRICT:
-       $doctype = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">';
+       $str = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">';
        break;
       case self::DOCTYPE_XHTML1_0_FRAMESET:
-       $doctype = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Frameset//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-frameset.dtd">';
+       $str = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Frameset//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-frameset.dtd">';
        break;
       case self::DOCTYPE_HTML5:
-       $doctype = '<!DOCTYPE html>';
+       $str = '<!DOCTYPE html>';
        break;
     }
-    return $doctype;
+    return $str;
+  }
+
+  /**
+   * Set the doctype according to the doctype passed to the function
+   *
+   * $doctype should be one in Pyrech\Layout::$doctypes_available
+   * 
+   * @param int $doctype
+   * @return string
+   */
+  public function setDoctype($doctype) {
+    if (in_array($doctype, self::$doctypes_available)) {
+      $this->doctype = $doctype;
+    }
+  }
+
+  /**
+   * Add a new element to the head
+   * 
+   * @param string $element
+   * @return string
+   */
+  public function addElement($element) {
+    $this->head[] = $element;
   }
 
   /**
@@ -80,7 +115,7 @@ class Layout {
    * you simply have to prefixe the key_value by 'my-key:'.
    *
    * Eg : if you need a meta refresh of 60, you should call this method like that :
-   * Layout::getMeta('http-equiv:refresh', 60)
+   * Pyrech\Layout::getMetaTag('http-equiv:refresh', 60)
    *
    * Or with the array $opts in render method : $opts['meta']['http-equiv:refresh'] = 60
    * 
@@ -88,7 +123,7 @@ class Layout {
    * @param string $content_value
    * @return string
    */
-  public function getMeta($key_value, $content_value) {
+  public function getMetaTag($key_value, $content_value) {
     $key_attribute = 'name';
     if (strpos($key_value, ':') !== false) {
       list($key_attribute, $key_value) = explode(':', $key_value, 2);
@@ -101,6 +136,48 @@ class Layout {
   }
 
   /**
+   * Add a meta tag to the head section
+   * 
+   * @see Pyrech\Layout::getMetaTag for details
+   * @param string $key_value
+   * @param string $content_value
+   * @return void
+   */
+  public function addMeta($key_value, $content_value) {
+    $this->addElement($this->getMetaTag($key_value, $content_value));
+  }
+
+  /**
+   * Return a link tag
+   * 
+   * $link is an array of attributes to set to the tag
+   * 
+   * @param array $link
+   * @return string
+   */
+  public function getLinkTag($link) {
+    if (!is_array($link)) {
+      return '';
+    }
+    $tag = '<link ';
+    foreach ($link as $key => $value) {
+      $tag .= $key.'="'.$value.'" ';
+    }
+    return $tag.'/>';
+  }
+
+  /**
+   * Add a link tag to the head section
+   * 
+   * @see Pyrech\Layout::getLinkTag for details
+   * @param array $link
+   * @return void
+   */
+  public function addLink($link) {
+    $this->addElement($this->getLinkTag($link));
+  }
+
+  /**
    * Return a link tag
    * 
    * $style can be a string and represent the href of the file and media by default is 'all',
@@ -109,7 +186,7 @@ class Layout {
    * @param mixed $style
    * @return string
    */
-  public function getStyle($style) {
+  public function getStyleTag($style) {
     if (is_array($style)) {
       $href  = $style['href'];
       $media = $style['media'];
@@ -121,7 +198,20 @@ class Layout {
     else {
       return '';
     }
-    return '<link href="'.$href.'" rel="stylesheet" media="'.$media.'" />';
+    return $this->getLinkTag(array('href'  => $href,
+                                   'rel'   => 'stylesheet',
+                                   'media' => $media));
+  }
+
+  /**
+   * Add a style tag to the head section
+   * 
+   * @see Pyrech\Layout::getStyleTag for details
+   * @param mixed $style
+   * @return void
+   */
+  public function addStyle($style) {
+    $this->addElement($this->getStyleTag($style));
   }
 
   /**
@@ -134,7 +224,7 @@ class Layout {
    * @param boolean $defer
    * @return string
    */
-  public function getScript($script, $defer) {
+  public function getScriptTag($script, $defer=false) {
     if (is_array($script) && array_key_exists('internal', $script)) {
       return '<script type="text/javascript">'.$script['internal'].'</script>';
     }
@@ -147,28 +237,124 @@ class Layout {
   }
 
   /**
+   * Add a script tag to the head section
+   * 
+   * @see Pyrech\Layout::getScriptTag for details
+   * @param mixed $script
+   * @param boolean $defer
+   * @return void
+   */
+  public function addScript($script, $defer=false) {
+    $this->addElement($this->getScriptTag($script, $defer));
+  }
+
+  /**
+   * Return a title tag
+   * 
+   * @param string $title
+   * @return string
+   */
+  public function getTitleTag($title) {
+    return '<title>'.$title.'</title>';
+  }
+
+  /**
+   * Add a title tag to the head section
+   * 
+   * @param string $title
+   * @return void
+   */
+  public function addTitle($title) {
+    $this->addElement($this->getTitleTag($title));
+  }
+
+  /**
+   * Return a icon tag
+   * 
+   * $style can be a string and represent the href of the file and media by default is 'all',
+   * or an array with a key 'href' and 'media' 
+   * 
+   * @param string $type
+   * @param string $href
+   * @return string
+   */
+  public function getIconTag($type, $href) {
+    switch ($type) {
+      case 'png':
+        return $this->getLinkTag(array('rel'  => 'icon',
+                                    'type' => 'image/png',
+                                    'href' => $href));
+        break;
+
+      case 'ico':
+        return $this->getLinkTag(array('rel'  => 'shortcut icon',
+                                    'type' => 'image/x-icon',
+                                    'href' => $href));
+        break;
+      
+      default:
+        return '';
+        break;
+    }
+  }
+
+  /**
+   * Add an icon to the head section
+   * 
+   * @see Pyrech\Layout::getStyleTag for details
+   * @param string $type
+   * @param string $href
+   * @return void
+   */
+  public function addIcon($type, $href) {
+    $this->addElement($this->getIconTag($type, $href));
+  }
+
+  /**
+   * Set body classnames
+   *
+   * $classes is an array of classnames or string for body
+   * 
+   * @param mixed $classes
+   * @return void
+   */
+  public function addBodyClass($classes) {
+    $str = '';
+    if (is_string($classes)) {
+      $str = $classes;
+    }
+    else if (is_array($classes)) {
+      $str = join(' ', $classes);
+    }
+    $this->body_classes = trim($this->body_classes.' '.$str);
+  }
+
+  /**
    * Return the top of the layout (doctype, head, open body)
    *
    * Include the doctype, open html tag, add meta, stylesheets, scripts in the head and open body tag.
    *
    * Options available :
    *
-   * doctype -> one of the DOCTYPE_X constant
-   * meta    -> array of meta. @see Layout::getMeta() for details
-   * title   -> title of the page
-   * styles  -> array of style. @see Layout::getStyle() for details
+   * doctype -> one of the DOCTYPE_X constant.@see Pyrech\Layout::setDoctype() for details
+   * meta    -> array of meta. @see Pyrech\Layout::getMetaTag() for details
+   * title   -> title of the page. @see Pyrech\Layout::getTitleTag() for details
+   * styles  -> array of style. @see Pyrech\Layout::getStyleTag() for details
    * icon    -> array with the url of favicon in .png and/or .ico
    *            Eg : $opts['icon'] = array('png' => '/favicon.png', 'ico' => '/favicon.ico');
-   * scripts -> array of script. @see Layout::getScript() for details
-   * defer   -> boolean. @see Layout::getScript() for details
-   * class   -> array of classnames or string for body
+   * scripts -> array of script. @see Pyrech\Layout::getScriptTag() for details
+   * defer   -> boolean. @see Pyrech\Layout::getScriptTag() for details
+   * class   -> mixed. @see Pyrech\Layout::addBodyClass() for details
    * 
    * @param string $key_value
    * @param string $content_value
    * @return string
    */
   private function getHead(array $opts) {
-    $head = $this->getDoctype(array_key_exists('doctype', $opts) ? $opts['doctype'] : self::DOCTYPE_HTML5);
+    if (array_key_exists('doctype', $opts)) {
+      $this->setDoctype($opts['doctype']);
+    }
+    $head = $this->getDoctypeTag();
     $head .= '
 <!--[if lte IE 7]><html class="no-js ie7 oldie" lang="fr"><![endif]-->
 <!--[if IE 8]><html class="no-js ie8 oldie" lang="fr"><![endif]-->
@@ -176,50 +362,39 @@ class Layout {
   <head>';
     if (array_key_exists('meta', $opts) && is_array($opts['meta'])) {
       foreach ($opts['meta'] as $name => $content) {
-        $head .= '
-    '.$this->getMeta($name, $content);
+        $this->addMeta($name, $content);
       }
     }
     if (array_key_exists('styles', $opts)) {
-      $head .='
-      <title>'.$opts['title'].'</title>';
+        $this->addTitle($name, $content);
     }
     if (array_key_exists('styles', $opts) && is_array($opts['styles'])) {
       foreach ($opts['styles'] as $style) {
-        $head .= '
-    '.$this->getStyle($style);
+        $this->addStyle($style);
       }
     }
 
     if (array_key_exists('icon', $opts) && is_array($opts['icon'])) {
-      if (array_key_exists('png', $opts)) {
-        $head .= '
-    <link rel="icon" type="image/png" href="'.$opts['icon']['png'].'">';
-      }
-      if (array_key_exists('ico', $opts)) {
-        $head .= '
-    <link rel="shortcut icon" type="image/x-icon" href="'.$opts['icon']['ico'].'">';
+      foreach ($opts['icon'] as $type => $icon) {
+        $this->addIcon($type, $icon);
       }
     }
     $defer = array_key_exists('defer', $opts) ? $opts['defer'] : false;
     if (array_key_exists('scripts', $opts) && is_array($opts['scripts'])) {
       foreach ($opts['scripts'] as $script) {
-        $head .= '
-    '.$this->getScript($script, $defer);
+        $this->addScript($script, $defer);
       }
     }
-    $classes = '';
     if (array_key_exists('class', $opts)) {
-      if (is_string($opts['class'])) {
-        $classes = $opts['class'];
-      }
-      else if (is_array($opts['class'])) {
-        $classes = join(' ', $opts['class']);
-      }
+      $this->addBodyClass($opts['class']);
+    }
+    foreach ($this->head as $value) {
+      $head .= '
+    '.$value;
     }
     $head .= '
   </head>
-  <body class="'.$classes.'">
+  <body class="'.$this->body_classes.'">
 ';
     return $head;
   }
